041be6fe0098648cc5125813bf0e912a42e13a33
allen.porter <allen.porter@58fa8ae0-6a36-0410-a7a1-b30e0234c60e>
2009-07-05 22:45:00 +0000

    Add some additional test cases for window resizing.
    git-svn-id: http://mobileterminal.googlecode.com/svn/branches/applesdk@373 58fa8ae0-6a36-0410-a7a1-b30e0234c60e

--- VT100/Tests/VT100Test.h
+++ VT100/Tests/VT100Test.h
@@ -20,5 +20,6 @@
 
 - (void) testBasicInput;
 - (void) testResize;
+- (void) testMultipleLinesWithResizing;
 
 @end
--- VT100/Tests/VT100Test.m
+++ VT100/Tests/VT100Test.m
@@ -32,6 +32,7 @@
   STAssertTrue(buffer[3].ch == '\0', @"expected '\0', got '%c'", buffer[0].ch);
 }
 
+// Tests basic window resizing
 - (void) testResize {
   ScreenSize size = [vt100 screenSize];
   STAssertEquals(80, size.width, @"expected 80, got %d", size.width);
@@ -46,6 +47,82 @@
   size = [vt100 screenSize];
   STAssertEquals(40, size.width, @"expected 40, got %d", size.width);
   STAssertEquals(20, size.height, @"expected 20, got %d", size.height);
+}
+
+static const int kLargeBufferSize = 8 * 1024;
+
+// Tests the case where we input data into the terminal, then resize the
+// window.  This causes some of the strings to change position.
+- (void) testMultipleLinesWithResizing {
+  char data[kLargeBufferSize];
+  
+  // First row is entirely a's
+  int i = 0;
+  for (int j = 0; j < 79; ++j) {
+    data[i++] = 'a';
+  }
+  data[i++] = '\r';
+  data[i++] = '\n';
+  // Second row is entirely b's
+  for (int j = 0; j < 79; ++j) {
+    data[i++] = 'b';
+  }
+  data[i++] = '\r';
+  data[i++] = '\n';
+  // Single c on the third row
+  data[i++] = 'c';
+  data[i++] = '\0';
+  [vt100 handleInputStream:data withLength:strlen(data)];
+    
+  // Verify the each row looks correct
+  screen_char_t* buffer = [vt100 bufferForRow:0];
+  for (int j = 0; j < 79; j++) {
+    STAssertTrue('a' == buffer[j].ch, @"got '%c'", buffer[j].ch);
+  }
+  STAssertTrue('\0' == buffer[79].ch, @"got '%c'", buffer[79].ch);
+  buffer = [vt100 bufferForRow:1];
+  for (int j = 0; j < 79; j++) {
+    STAssertTrue('b' == buffer[j].ch, @"got '%c'", buffer[j].ch);
+  }
+  STAssertTrue('\0' == buffer[79].ch, @"got '%c'", buffer[79].ch);
+  buffer = [vt100 bufferForRow:2];
+  STAssertTrue('c' == buffer[0].ch, @"got '%c'", buffer[0].ch);
+  STAssertTrue('\0' == buffer[1].ch, @"got '%c'", buffer[1].ch);
+  
+
+  // Change the size of the screen, which causes the terminal to move
+  // everything.
+  ScreenSize size = [vt100 screenSize];
+  size.width = 40;
+  size.height = 25;
+  [vt100 setScreenSize:size];
+  
+  // Now the first two (shorter) rows are a's
+  buffer = [vt100 bufferForRow:0];
+  for (int j = 0; j < 40; j++) {
+    STAssertTrue('a' == buffer[j].ch, @"buffer[%d] was '%c'", j, buffer[j].ch);
+  }
+  buffer = [vt100 bufferForRow:1];
+  for (int j = 0; j < 39; j++) {
+    STAssertTrue('a' == buffer[j].ch, @"buffer[%d] was'%c'", j, buffer[j].ch);
+  }
+  STAssertTrue('\0' == buffer[39].ch, @"was '%c'", buffer[39].ch);
+  
+  // Next two rows are b's
+  buffer = [vt100 bufferForRow:2];
+  for (int j = 0; j < 40; j++) {
+    STAssertTrue('b' == buffer[j].ch, @"buffer[%d] was '%c'", j, buffer[j].ch);
+  }
+  buffer = [vt100 bufferForRow:3];
+  for (int j = 0; j < 39; j++) {
+    STAssertTrue('b' == buffer[j].ch, @"buffer[%d] was'%c'", j, buffer[j].ch);
+  }
+  STAssertTrue('\0' == buffer[39].ch, @"was '%c'", buffer[39].ch);
+  
+  // Last row has a single c
+  buffer = [vt100 bufferForRow:4];
+  STAssertTrue('c' == buffer[0].ch, @"got '%c'", buffer[0].ch);
+  STAssertTrue('\0' == buffer[1].ch, @"got '%c'", buffer[1].ch);
 }
 
 @end
